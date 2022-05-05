9790bb771c2b510ca4065825fadf8e58084d36df
Breno Rodrigues <brenocartoon@hotmail.com>
2022-05-04 23:41:29 -0300

    Create POST route to feedbacks

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import { prisma } from './prisma';
+import HttpStatusCode from './utils/HttpStatusCode';
 
 const app = express();
 
 const PORT = 3001;
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}.`);
@@ -10,8 +14,22 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
     return res.send('Hello traveler...')
-})
+});
 
 app.get('/feedbacks', (req, res) => {
     return res.send('Welcome to feedback page...');
-})
+});
+
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const newFeedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    });
+    
+    return res.status(HttpStatusCode.CREATED).json(newFeedback);
+});
