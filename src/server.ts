84f04abd6608bb3a15a2e622f5e49a61ae7e2f62
Breno Rodrigues <brenocartoon@hotmail.com>
2022-05-10 22:11:18 -0300

    Remove all unnecessary info from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { env } from 'process';
 
-import { prisma } from './prisma';
-import HttpStatusCode from './utils/HttpStatusCode';
+import { routes } from './routes';
 
 const app = express();
 
@@ -11,49 +8,8 @@ const PORT = 3001;
 
 app.use(express.json());
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: env.MAILER_USER,
-        pass: env.MAILER_PASS
-    }
-});
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}.`);
-});
-
-app.get('/', (req, res) => {
-    return res.send('Hello traveler...')
-});
-
-app.get('/feedbacks', (req, res) => {
-    return res.send('Welcome to feedback page...');
-});
-
-app.post('/feedbacks', async (req, res) => {
-    const { type, comment, screenshot } = req.body;
-
-    const newFeedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    });
-
-    await transport.sendMail({
-        from: "Feedget <contato@feedget.net",
-        to: "Breno <mrodrigues.breno@gmail.com",
-        subject: "Dado criado",
-        html: [
-            `<div>`,
-            `<p>Tipo de feedback: ${type}</p>`,
-            `<p>Seu comentário: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
-    });
-    
-    return res.status(HttpStatusCode.CREATED).json(newFeedback);
 });
